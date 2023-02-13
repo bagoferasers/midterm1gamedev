4e6fce0d6e69dd5bd4707b28146bc47dc86f19ce
bagoferasers <ctrull60@gmail.com>
2023-02-13 17:26:25 -0600

    added upon enter
    yes

--- Midterm1/Assets/Character.cs
+++ Midterm1/Assets/Character.cs
@@ -6,12 +6,15 @@ public class Character : MonoBehaviour
 {
     [ Header( "Movement" ) ]
     public float speed;
+
+    float playerHealth;
     Rigidbody2D rb2d;
     
     // Start is called before the first frame update
     void Start( )
     {
         rb2d = GetComponent< Rigidbody2D >( );
+        playerHealth = 50f;
     }
 
     // Update is called once per frame
@@ -23,5 +26,15 @@ public class Character : MonoBehaviour
     void FixedUpdate( )
     {
         transform.position += new Vector3(Input.GetAxisRaw("Horizontal"),0,0) * speed * Time.deltaTime;    
+    }
+
+    private void OnTriggerEnter2D( Collider2D collider )
+    {
+        if( collider.tag == "healthRegen" && playerHealth < 100 )
+        {
+            Destroy( collider.gameObject );
+            Debug.Log( "Collided with healthRegen" );
+            playerHealth += 10;
+        }
     }
 }
