960f92da0bf06a881ff4a55a8023ad859d1fab1c
bagoferasers <ctrull60@gmail.com>
2023-02-13 15:01:15 -0600

    random grass placed
    yes!

--- Midterm1/Assets/Character.cs
+++ Midterm1/Assets/Character.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    [ Header( "Movement" ) ]
+    public float speed;
+    Rigidbody2D rb2d;
+    
     // Start is called before the first frame update
-    void Start()
+    void Start( )
     {
-        
+        rb2d = GetComponent< Rigidbody2D >( );
     }
 
     // Update is called once per frame
-    void Update()
+    void Update( )
     {
         
+    }
+    
+    void FixedUpdate( )
+    {
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"),0,0) * speed * Time.deltaTime;    
     }
 }
