739028b5a83de6b8e9b22647feab52cfec59ddb2
bagoferasers <ctrull60@gmail.com>
2023-02-14 11:51:43 -0600

    a
    a

--- Midterm1/Assets/boltGen.cs
+++ Midterm1/Assets/boltGen.cs
@@ -7,12 +7,15 @@ public class boltGen : MonoBehaviour
     [ Header( "enter object to generate" ) ]
     public GameObject g;
 
+    [ Header( "enter object to start at" ) ]
+    public GameObject s;
+
     // Start is called before the first frame update
     void Start( )
     {
-        GameObject i = Instantiate( g, new Vector3( 0, 0, 0 ), Quaternion.identity );
+        GameObject i = Instantiate( g, new Vector3( s.transform.position.x, s.transform.position.y, s.transform.position.z ), Quaternion.identity );
         i.transform.parent = gameObject.transform;
-        //generateMe( );
+        generateMe( );
         
     }
 
@@ -29,8 +32,8 @@ public class boltGen : MonoBehaviour
     
     public IEnumerator gen( )
     {
-        yield return new WaitForSeconds( 3 );
-        GameObject i = Instantiate( g, new Vector3( 0, 0, 0 ), Quaternion.identity );      
+        yield return new WaitForSeconds( 5 );
+        GameObject i = Instantiate( g, new Vector3( s.transform.position.x, s.transform.position.y, s.transform.position.z ), Quaternion.identity  );      
         i.transform.parent = gameObject.transform;
         generateMe( );
     }   
